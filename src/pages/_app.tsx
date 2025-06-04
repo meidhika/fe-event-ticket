d01c35f58b0165c3e1b79d09d05e40fb7ff12faf
meidhika <meidhikanawa@gmail.com>
2025-06-04 21:33:16 +0700

    Setup Register Page Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
+import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300","400", "500", "600", "700", "800", "900"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <NextUIProvider>
+      <main className={cn(inter.className,"flex min-h-screen flex-col items-center justify-center py-10 lg:py gap-10 min-w-full")}>
+
        <Component {...pageProps} />
+      </main>
     </NextUIProvider>
   );
 }
