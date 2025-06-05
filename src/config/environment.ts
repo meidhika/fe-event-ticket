7c9eb4541c0c7662d3633f558fd401f152b8f08a
meidhika <meidhikanawa@gmail.com>
2025-06-05 20:56:36 +0700

    setup Next Auth

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,5 +1,6 @@
 const environmnet ={
     API_URL: process.env.NEXT_PUBLIC_API_URL,
+    AUTH_SECRET: process.env.NEXTAUTH_SECRET,
 }
 
 export default environmnet
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 
@@ -20,9 +21,10 @@ const queryClient = new QueryClient({
   },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return(
-    <QueryClientProvider client={queryClient}>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
       <NextUIProvider>
       <main className={cn(inter.className,"flex min-h-screen flex-col items-center justify-center py-10 lg:py gap-10 min-w-full")}>
 
@@ -30,6 +32,8 @@ export default function App({ Component, pageProps }: AppProps) {
       </main>
     </NextUIProvider>
     </QueryClientProvider>
+    </SessionProvider>
+    
     
   );
 }
