8af19954fdcd49d6251eac5949c6fa4a93dc5f9c
meidhika <meidhikanawa@gmail.com>
2025-06-06 15:22:54 +0700

    Setup UI Layout Dashboard

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,21 +1,20 @@
-import PageHead from "../../commons/PageHead/PageHead"
+import PageHead from "../../commons/PageHead/PageHead";
 import { Fragment, ReactNode } from "react";
 
-interface PropTypes{
-    children: ReactNode;
-    title?: string;
+interface PropTypes {
+  children: ReactNode;
+  title?: string;
 }
 const AuthLayout = (props: PropTypes) => {
-    const {children, title } = props;
-    return (
-        <Fragment>
-            <PageHead title={title}/>
-            <section className="max-w-screen-3xl 3xl:containet p-6">
-                {children}
-            </section>
-        </Fragment>
-    )
-}   
-
+  const { children, title } = props;
+  return (
+    <div className="lg:py flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10">
+      <PageHead title={title} />
+      <section className="max-w-screen-3xl 3xl:containet p-6">
+        {children}
+      </section>
+    </div>
+  );
+};
 
 export default AuthLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,9 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 
-
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["100", "200", "300","400", "500", "600", "700", "800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const queryClient = new QueryClient({
@@ -21,19 +20,19 @@ const queryClient = new QueryClient({
   },
 });
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
-  return(
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
-      <NextUIProvider>
-      <main className={cn(inter.className,"flex min-h-screen flex-col items-center justify-center py-10 lg:py gap-10 min-w-full")}>
-
-       <Component {...pageProps} />
-      </main>
-    </NextUIProvider>
-    </QueryClientProvider>
+        <NextUIProvider>
+          <main className={cn(inter.className)}>
+            <Component {...pageProps} />
+          </main>
+        </NextUIProvider>
+      </QueryClientProvider>
     </SessionProvider>
-    
-    
   );
 }
