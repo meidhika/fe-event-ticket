c2de203743a553856092a964a6bcd00a69e3fab6
meidhika <meidhikanawa@gmail.com>
2025-06-19 20:34:36 +0700

    Feat: Category List

--- src/components/views/Home/Home.tsx
+++ src/components/views/Home/Home.tsx
@@ -3,6 +3,8 @@ import { Skeleton } from "@nextui-org/react";
 import HomeSlider from "./HomeSlider";
 import useHome from "./useHome";
 import Image from "next/image";
+import HomeEventList from "./HomeEventList";
+import HomeCategoryList from "./HomeCategoryList";
 // import HomeCategoryList from "./HomeCategoryList";
 
 const Home = () => {
@@ -21,6 +23,32 @@ const Home = () => {
       <HomeSlider
         banners={dataBanners?.data}
         isLoadingBanners={isLoadingBanners}
+      />
+      <HomeEventList
+        title="Featured Event"
+        events={dataFeaturedEvents?.data}
+        isLoading={isLoadingFeaturedEvents}
+      />
+      <Skeleton
+        isLoaded={!isLoadingBanners}
+        className="mb-16 h-[20vw] w-full rounded-2xl px-6 lg:px-0"
+      >
+        <Image
+          src={dataBanners && dataBanners?.data[1]?.image}
+          alt="banner"
+          className="h-[20vw] w-full rounded-2xl object-cover object-center"
+          width={1920}
+          height={800}
+        />
+      </Skeleton>
+      <HomeEventList
+        title="Latest Event"
+        events={dataLatestEvents?.data}
+        isLoading={isLoadingLatestEvents}
+      />
+      <HomeCategoryList
+        categories={dataCategories?.data}
+        isLoading={isLoadingCategories}
       />
     </div>
   );
